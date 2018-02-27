8e90e9cd46b2437020cd99ec213d13c574089c09
faheemsiddiq <faheem.siddiqk@gmail.com>
2018-02-27 18:51:34 +1100

    version 3.12.3 up to date

--- app/src/main/java/com/sinch/contactchooser/ChooseContact.java
+++ app/src/main/java/com/sinch/contactchooser/ChooseContact.java
@@ -1,12 +1,17 @@
 package com.sinch.contactchooser;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.provider.ContactsContract;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Base64;
 import android.util.Log;
 import android.view.View;
@@ -19,21 +24,43 @@ import org.apache.http.client.ResponseHandler;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.BasicResponseHandler;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.security.cert.CertificateFactory;
 
-public class ChooseContact extends ActionBarActivity {
+import javax.net.ssl.HttpsURLConnection;
+
+import static android.R.attr.password;
+
+public class ChooseContact extends AppCompatActivity {
 
     public final int PICK_CONTACT = 2015;
+    private static final String APP_KEY = "enter-application-key";
+    private static final String APP_SECRET = "enter-application-secret";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_contact);
 
-        (findViewById(R.id.button)).setOnClickListener( new View.OnClickListener() {
+        if (ContextCompat.checkSelfPermission(ChooseContact.this, Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(ChooseContact.this,
+                    new String[]{Manifest.permission.READ_CONTACTS},
+                    1);
+        }
+
+        (findViewById(R.id.button)).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent i = new Intent(Intent.ACTION_PICK, ContactsContract.CommonDataKinds.Phone.CONTENT_URI);
@@ -55,9 +82,6 @@ public class ChooseContact extends ActionBarActivity {
 
     class normalizePhoneNumberTask extends AsyncTask<String, Void, String> {
 
-        private String appKey = "your_app_key";
-        private String appSecret = "your_app_secret";
-
         @Override
         protected String doInBackground(String... params) {
 
@@ -65,9 +89,9 @@ public class ChooseContact extends ActionBarActivity {
 
             try {
                 HttpClient httpclient = new DefaultHttpClient();
-                HttpGet httpGet = new HttpGet("https://callingapi.sinch.com/v1/calling/query/number/" + params[0].replaceAll("\\s+",""));
+                HttpGet httpGet = new HttpGet("https://callingapi.sinch.com/v1/calling/query/number/" + params[0].replaceAll("\\s+", ""));
 
-                String usernamePassword = "application:" + appKey + ":" + appSecret;
+                String usernamePassword = "application:" + APP_KEY + ":" + APP_SECRET;
                 String encoded = Base64.encodeToString(usernamePassword.getBytes(), Base64.NO_WRAP);
                 httpGet.addHeader("Authorization", "Basic " + encoded);
 
